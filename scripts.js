d45313e1c386f4a9ba37402a6b30d0893b94270a
Masha Ivy <mashaivy9@gmail.com>
2019-08-23 09:06:19 -0700

    add few things (user int logic) to js file

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//user int logic
+$(document).ready(function(){
+
+$("form#enterNumber").submit(function(event)) {
+    //unhide responces
+var number=parseInt($("input#anynum")).val()
+var result = numberRange
+})
+})
+
+//bussiness
