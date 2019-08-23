cc20a0a1b005ab6d85adc04cdfd759ea1cc82ac5
Masha Ivy <mashaivy9@gmail.com>
2019-08-23 13:41:26 -0700

    try different things

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 //user int logic
+
+
 $(document).ready(function(){
 
 $("form#enterNumber").submit(function(event) {
     event.preventDefault();
-    var numberInput = ($("input").split(" ").val()
+    var numberInput = $("input").val();
 
+    var newArray = [];
 
+    newArray.push(numberInput);
 
+    console.log(numberInput);
 
-
-
-
-var newArray = [];
-newArray.push(numberInput)
-
-console.log(newArray)
+    for (i=0; i=newArray.length; i++) {
+    if (numberInput.includes("1"))
+    "result1".show }
 //unhide responces
 })
 })
 
 //bussiness
-if
