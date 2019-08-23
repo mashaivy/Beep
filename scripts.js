ad545ca7270f6b35da8c771341934140b5239729
Masha Ivy <mashaivy9@gmail.com>
2019-08-23 12:43:32 -0700

    will show that I'm actually working on it but nothing works and I don't I can;t even figure out how to split a number input

--- scripts.js
+++ scripts.js
@@ -3,16 +3,21 @@ $(document).ready(function(){
 
 $("form#enterNumber").submit(function(event) {
     event.preventDefault();
-    var numberInput = $("input").val().split(" ");
+    var numberInput = ($("input").split(" ").val()
+
+
+
 
 
 
 
 var newArray = [];
 newArray.push(numberInput)
+
 console.log(newArray)
 //unhide responces
 })
 })
 
 //bussiness
+if
