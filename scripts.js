f5a41e8cba080286ddf8ae2e10ee4a6ffa5e4ba1
Masha Ivy <mashaivy9@gmail.com>
2019-08-23 16:53:37 -0700

    add final js with if statements that make an alert when entered a number

--- scripts.js
+++ scripts.js
@@ -1,26 +1,24 @@
 //user int logic
-
-
 $(document).ready(function(){
-
-$("form#enterNumber").submit(function(event) {
+    $("form#enterNumber").submit(function(event) {
+    event.preventDefault();
     $("result1").show();
     var numberInput = $("input").val();
-
     var newArray = [];
-
     newArray.push(numberInput);
-
     for (i=0; i=newArray.length; i++) {
-    if (numberInput[i].includes("1")) {
-        $("#responces1").append("boop"); //why doesnt it work?
-    }else {
-        alert ("no");
+     if (numberInput[i].includes("3")||numberInput[i]==="3"){
+    alert ("I'm sorry, Dave. I'm afraid I can't do that.")
+         //$("#responces1").append("boop"); why doesnt it work?
+         //$("#result").show(); didn't work either
+    }else if (numberInput[i].includes("2")||numberInput[i]==="2") {
+    alert ("Your number input is: Boop!")
+    }else if (numberInput[i].includes("1")||numberInput[i]==="1") {
+    alert ("your number input is: Beep!")
+    }
     }
 
+});
+});
 
-    event.preventDefault();
-};
-});
-});
 //bussiness
