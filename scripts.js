a5b0d8ae36ec9d5074fb2980281da7e847ab6066
Masha Ivy <mashaivy9@gmail.com>
2019-08-23 15:43:17 -0700

    try different method

--- scripts.js
+++ scripts.js
@@ -4,20 +4,22 @@
 $(document).ready(function(){
 
 $("form#enterNumber").submit(function(event) {
-    event.preventDefault();
     $("result1").show();
     var numberInput = $("input").val();
 
     var newArray = [];
 
     newArray.push(numberInput);
-    console.log(newArray)
-    for (i=0; i=newArray.length; i++) {
-    if (numberInput[i].includes("1")); {
 
+    for (i=0; i=newArray.length; i++) {
+    if (numberInput[i].includes("1")) {
+        $("#responces1").append("boop"); //why doesnt it work?
+    }else {
+        alert ("no");
     }
 
 
+    event.preventDefault();
 };
 });
 });
