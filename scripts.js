7c92029790561e93b1c1b91299853dedd27bfbdf
Masha Ivy <mashaivy9@gmail.com>
2019-08-23 19:33:05 -0700

    figured out why it didn't workgit add .git add .git add .git add .git add .1 i should be less then array length not equal

--- scripts.js
+++ scripts.js
@@ -1,24 +1,32 @@
-//user int logic
+
+    //back-end
+//front-end logic
 $(document).ready(function(){
     $("form#enterNumber").submit(function(event) {
     event.preventDefault();
-    $("result1").show();
     var numberInput = $("input").val();
+
     var newArray = [];
     newArray.push(numberInput);
-    for (i=0; i=newArray.length; i++) {
-     if (numberInput[i].includes("3")||numberInput[i]==="3"){
-    alert ("I'm sorry, Dave. I'm afraid I can't do that.")
-         //$("#responces1").append("boop"); why doesnt it work?
-         //$("#result").show(); didn't work either
-    }else if (numberInput[i].includes("2")||numberInput[i]==="2") {
-    alert ("Your number input is: Boop!")
-    }else if (numberInput[i].includes("1")||numberInput[i]==="1") {
-    alert ("your number input is: Beep!")
-    }
-    }
+    var resultArray = [];
+
+    var sorry = "srry dave"
+
+        for (i=0; i<newArray.length; i++) {
+            if (numberInput[i].includes("3")||numberInput[i]==="3"){
+                //resultArray.push(sorry);
+                //console.log(resultArray)
+                //alert ("I'm sorry, Dave. I'm afraid I can't do that.")
+                $("#responces1").append("boop"); 
+                //$("#result").show(); didn't work either
+            }else if (numberInput[i].includes("2")||numberInput[i]==="2") {
+                alert ("Your number input is: Boop!")
+            }else if (numberInput[i].includes("1")||numberInput[i]==="1") {
+                alert ("your number input is: Beep!")
+            };
+        };
 
 });
 });
 
-//bussiness
+//
