04ff1efacf0015f2d287382fff5329d2b2bcca8b
Masha Ivy <mashaivy9@gmail.com>
2019-08-23 20:59:27 -0700

    add some css styling and a picture

--- scripts.js
+++ scripts.js
@@ -1,32 +1,27 @@
-
-    //back-end
-//front-end logic
+//UI logic
 $(document).ready(function(){
     $("form#enterNumber").submit(function(event) {
-    event.preventDefault();
-    var numberInput = $("input").val();
+     event.preventDefault();
+     var numberInput = $("input").val();
+     var newArray = [];
+     newArray.push(numberInput);
 
-    var newArray = [];
-    newArray.push(numberInput);
-    var resultArray = [];
 
-    var sorry = "srry dave"
+//Bussiness logic
+    for (i=0; i<newArray.length; i++) {
+        if (numberInput[i].includes("3")||numberInput[i]==="3"){
+        $("#responces1").append("I'm sorry, Dave. I'm afraid I can't do that.");
+        }else if (numberInput[i].includes("2")||numberInput[i]==="2") {
+        $("#responces1").append("boop!");
+        }else if (numberInput[i].includes("1")||numberInput[i]==="1") {
+        $("#responces1").append("beep!");
+    } else {
+        $("#noResult").show();
+    }
 
-        for (i=0; i<newArray.length; i++) {
-            if (numberInput[i].includes("3")||numberInput[i]==="3"){
-                //resultArray.push(sorry);
-                //console.log(resultArray)
-                //alert ("I'm sorry, Dave. I'm afraid I can't do that.")
-                $("#responces1").append("I'm sorry, Dave. I'm afraid I can't do that.");
-                //$("#result").show(); didn't work either
-            }else if (numberInput[i].includes("2")||numberInput[i]==="2") {
-                $("#responces1").append("boop!");
-            }else if (numberInput[i].includes("1")||numberInput[i]==="1") {
-                    $("#responces1").append("beep!");
-            };
-        };
-
+};
 });
+
 });
 
 //
