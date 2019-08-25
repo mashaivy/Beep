3adf9173b3ea6deacd8758f53921227baa24721a
Masha Ivy <mashaivy9@gmail.com>
2019-08-24 18:56:47 -0700

    add more stile elements

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
 //UI logic
 $(document).ready(function(){
     $("form#enterNumber").submit(function(event) {
-     event.preventDefault();
-     var numberInput = $("input").val();
-     var newArray = [];
-     newArray.push(numberInput);
+        event.preventDefault();
+        $(".bubble").hide()
+        var numberInput = $("input").val();
+        var newArray = [];
+        newArray.push(numberInput);
      //console.log(newArray)
 //Bussiness logic
-    for (i=0; i<newArray.length; i++) {
-        if (newArray[i].includes("3")) {
-        $("#responces1").append("I'm sorry, Dave. I'm afraid I can't do that.");
-    }else if (newArray[i].includes("2")) {
-        $("#responces1").append("boop!");
-    }else if (newArray[i].includes("1")) {
-        $("#responces1").append("beep!");
-    } else {
-        $("#noResult").show();
+        for (i=0; i<newArray.length; i++) {
+            if (newArray[i].includes("3")) {
+                $("#responces1").append("I'm sorry, Dave. I'm afraid I can't do that.");
+            }else if (newArray[i].includes("2")) {
+                $("#responces1").append("boop!");
+            }else if (newArray[i].includes("1")) {
+                $("#responces1").append("beep!");
+            } else {
+                $("#noResult").show();
     };
 };
 });
