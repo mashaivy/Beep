972fdf200cff1f36484f6a23026990d4b2020a03
Masha Ivy <mashaivy9@gmail.com>
2019-08-30 08:32:41 -0700

    make a robot return numbers to a user

--- scripts.js
+++ scripts.js
@@ -3,21 +3,30 @@ $(document).ready(function(){
     $("form#enterNumber").submit(function(event) {
         event.preventDefault();
         $(".bubble").hide()
-        var numberInput = $("input").val();
-        var newArray = [];
-        newArray.push(numberInput);
-     //console.log(newArray)
-//Bussiness logic
-        for (i=0; i<newArray.length; i++) {
-            if (newArray[i].includes("3")) {
-                $("#responces1").append("I'm sorry, Dave. I'm afraid I can't do that.");
-            }else if (newArray[i].includes("2")) {
-                $("#responces1").append("boop!");
-            }else if (newArray[i].includes("1")) {
-                $("#responces1").append("beep!");
+        var numberInput = parseInt($("input").val());
+        var result = beepBoop(numberInput)
+        $("#responces1").append(result)
+        console.log(newArray)
+
+    });
+});
+
+
+            function beepBoop(numberInput) {
+            for (var i=0; i<=numberInput; i++) {
+
+             if (i.toString().includes("3")) {
+                newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+            } else if (i.toString().includes("2")) {
+                newArray.push("boop!")
+            } else if (i.toString().includes("1")) {
+                newArray.push("beep!")
             } else {
-                $("#noResult").show();
+                newArray.push(i);
+
+            }
+
+    };            //ORvar newArray = [];
+    return newArray;
     };
-};
-});
-});
+    var newArray = [];
