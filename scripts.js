b1fdefe118a68db96962623ee472883edb7e6ed3
Masha Ivy <mashaivy9@gmail.com>
2019-08-30 18:55:58 -0700

    resubmit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-//UI logic
 $(document).ready(function(){
     $("form#enterNumber").submit(function(event) {
         event.preventDefault();
@@ -10,23 +9,18 @@ $(document).ready(function(){
 
     });
 });
-
-
-            function beepBoop(numberInput) {
-            for (var i=0; i<=numberInput; i++) {
-
-             if (i.toString().includes("3")) {
-                newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+function beepBoop(numberInput) {
+    for (var i=0; i<=numberInput; i++) {
+         if (i.toString().includes("3")) {
+            newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
             } else if (i.toString().includes("2")) {
                 newArray.push("boop!")
             } else if (i.toString().includes("1")) {
                 newArray.push("beep!")
             } else {
                 newArray.push(i);
-
             }
-
-    };            //ORvar newArray = [];
+    };
     return newArray;
     };
     var newArray = [];
